220c542dd9694f202c698958f4aeb23c58b564bc
Intellectus7 <adanjokanma@gmail.com>
2026-06-09 05:57:41 +0100

    Update answer.py

--- backend/bots/Nexus/answer.py
+++ backend/bots/Nexus/answer.py
@@ -6,12 +6,15 @@ load_dotenv()
 client = Groq()  
 
 def answer_query(question):
-    chat_completions = client.chat.completions.create(
-        messages = [{
-            'role': 'user',
-            'content': question
-
-        }],
-        model = "llama-3.3-70b-versatile",
-    )
-    return chat_completions.choices[0].message.content
+    try:
+        chat_completions = client.chat.completions.create(
+            messages = [{
+                'role': 'user',
+                'content': question
+    
+            }],
+            model = "llama-3.3-70b-versatile",
+        )
+        return chat_completions.choices[0].message.content
+    except Exception as error:
+        return f"GROQ ERROR: {str(error)}"
